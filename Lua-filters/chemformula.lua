47955ecb0a2d09a2191ab5ffd61af355789b3365
Gonzalo Esteban <eolo.sensei@gmail.com>
2021-03-08 13:41:15 +0100

    Fixed chemformula filter

--- Lua-filters/chemformula.lua
+++ Lua-filters/chemformula.lua
@@ -1,24 +1,12 @@
 if FORMAT:match 'latex' then
     function Str (elem)
-        i, j, sign = elem.text:find("([%p%c]+)$")
-        if sign then
-            formula = elem.text:sub(1, i-1)
-        else
-            sign = ""
-            formula = elem.text
+        if elem.text:match("%u%l?%d?%)?%d") then
+            start, formula, stop = elem.text:match("^(%p*)([%w%(%)%-%+=]+%w)(%p*)$")
+            -- print(elem.text,":",start, formula, stop)
+            return { pandoc.Str(start),
+                    pandoc.RawInline('latex', '\\ch{' .. formula .. '}'),
+                    pandoc.Str(stop)}
         end
-        
-        if formula:match("%(?[A-Z][a-z]?%d?%)?%d+") then
-            return pandoc.RawInline('latex', "\\ch{" .. formula .. "}" .. sign)
-        else
-            return elem
-        end
+        return nil
     end
 end
-
--- local mono_compounds = "^[A-Z][a-z]?%d*$"
--- local binary_compounds = "^[A-Z][a-z]?%d*[A-Z][a-z]?%d*$"
--- local ternary_compounds = "^[A-Z][a-z]?%d*%(?[A-Z][a-z]?%d*[A-Z][a-z]?%d*%)?%d*$"
--- local all = "^[A-Z][a-z]?%d*%(?.*%)?%d*$" -- pero coge también palabras con mayúscula
-
--- local posible_global = "^[A-Z][a-z]?%L+"
